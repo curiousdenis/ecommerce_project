d54bfda8da535f3deee98b6a490cc3cf2b5df9ab
curiousdenis <torooopov99@gmail.com>
2025-01-29 14:34:49 +0700

    First commit

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, status
 from typing import Annotated, Optional, Generator
 from sqlmodel import Field, Session, SQLModel, create_engine
 
-class Customer(SQLModel, table = True):
-    id: Optional[int] = Field(default = None, primary_key=True)
+class CustomerBase(SQLModel):
     login: str = Field(index=True)
     name: Optional[str] = Field(default = None)
     surname: Optional[str] = Field(default = None, index = True)
 
+class Customer(CustomerBase, table = True):
+    id: Optional[int] = Field(default = None, primary_key=True)
+
     def __str__(self) -> str:
         return '{} with name = {} and surname = {}. Your id is {}'.\
-            format(type(self).__name__, self.name, self.surname,self.id )
+            format(type(self).__name__, self.name, self.surname,self.id)
+
+class CustomerCreate(CustomerBase):
+    pass
 
 
 sqlite_file_name = "database.db"
@@ -39,7 +44,7 @@ def read_customer(customer_id: int, session:SessionDep) -> Customer:
         raise HTTPException(status_code=404, detail='Such customer does not exist')
     else:
         return customer
-@app.post("/create_customer/")
+@app.post("/create_customer/", status_code=status.HTTP_201_CREATED)
 def create_customer(customer: Customer, session: SessionDep) -> Customer:
     session.add(customer)
     session.commit()
