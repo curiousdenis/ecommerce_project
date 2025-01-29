38e882dd70a2bc25edf498d334410e87ba88fff2
curiousdenis <torooopov99@gmail.com>
2025-01-29 17:35:17 +0700

    First commit

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class CustomerBase(SQLModel):
     surname: Optional[str] = Field(default = None, index = True)
 
 class Customer(CustomerBase, table = True):
+    """It will be returned to user"""
     id: Optional[int] = Field(default = None, primary_key=True)
 
     def __str__(self) -> str:
@@ -15,7 +16,13 @@ class Customer(CustomerBase, table = True):
             format(type(self).__name__, self.name, self.surname,self.id)
 
 class CustomerCreate(CustomerBase):
+    """It is a model to show user what they shood provide to create customer"""
     pass
+
+class CustomerUpdate(CustomerBase):
+    login: Optional[str]
+    name: Optional[str]
+    surname: Optional[str]
 
 
 sqlite_file_name = "database.db"
@@ -37,16 +44,29 @@ app = FastAPI()
 def on_start_up() -> None:
     create_db_and_tables()
 
-@app.get("/get_customer/{customer_id}")
-def read_customer(customer_id: int, session:SessionDep) -> Customer:
+@app.get("/get_customer/{customer_id}", status_code=status.HTTP_200_OK, response_model=Customer)
+def read_customer(customer_id: int, session:SessionDep):
     customer = session.get(Customer, customer_id)
     if not customer:
         raise HTTPException(status_code=404, detail='Such customer does not exist')
     else:
         return customer
-@app.post("/create_customer/", status_code=status.HTTP_201_CREATED)
-def create_customer(customer: Customer, session: SessionDep) -> Customer:
+@app.post("/create_customer/", status_code=status.HTTP_201_CREATED, response_model=Customer)
+def create_customer(customer_input: CustomerCreate, session: SessionDep):
+    customer = Customer.model_validate(customer_input) #here we validate user input (which is itself CustomerCreate) against Customer
     session.add(customer)
+    session.commit()
+    session.refresh(customer)
+    return customer
+
+@app.patch("/update_customer/{customer_id}", status_code=status.HTTP_200_OK, response_model=Customer)
+def update_customer(customer_id: int, customer_input: CustomerUpdate, session: SessionDep):
+    customer = session.get(Customer, customer_id)
+    if not customer:
+        raise HTTPException(status_code=404, detail='Such customer does not exist')
+    else:
+        for key, value in customer_input.dict(exclude_unset = True).items():
+            setattr(customer, key, value)
     session.commit()
     session.refresh(customer)
     return customer
