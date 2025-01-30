650e3467990b83b4766cbaed8fcc292d74c389b6
curiousdenis <torooopov99@gmail.com>
2025-01-30 14:27:28 +0700

    Changes of the structure

--- dependency.py
+++ dependency.py
@@ -1,6 +1,6 @@
 from typing import Annotated
-from .main import get_session
 from sqlmodel import Session
 from fastapi import Depends
+from sql_engine import get_session
 
 SessionDep = Annotated[Session, Depends(get_session)]
--- main.py
+++ main.py
@@ -1,24 +1,11 @@
 from fastapi import FastAPI
-from typing import Generator
-from sqlmodel import Session, SQLModel, create_engine
-import routers
-
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
-
-def create_db_and_tables() -> None:
-    SQLModel.metadata.create_all(engine)
-
-def get_session() -> Generator[Session, None, None]:
-    with Session(engine) as session:
-        yield session
+from routers.customer import router as customer_router
+import sql_engine
 
 app = FastAPI()
-app.include_router(routers.customer.router)
+app.include_router(customer_router)
 
 @app.on_event("startup")
 def on_start_up() -> None:
-    create_db_and_tables()
+    sql_engine.create_db_and_tables()
 
